383ad5c146f83c0399dddc377c974f6a1012cc82
Marco Vavassori <mv@marcovavassori.com>
2025-06-02 21:51:07 +0200

    add comment for clarity

--- Repositories/BaseRepository.php
+++ Repositories/BaseRepository.php
@@ -6,14 +6,14 @@ use Database\Connection;
 
 abstract class BaseRepository {
     protected PDO $db;
-    
+
     public function __construct() {
-        // get the singleton database connection
+        // get the singleton database connection, i.e. every repository gets the same connection instance
         $this->db = Connection::getInstance();
     }
-    
+
     // A helper method for executing queries with parameters
-    protected function execute(string $query, array $params = []): bool | \PDOStatement { // `\` means from the global namespace
+    protected function execute(string $query, array $params = []): bool|\PDOStatement { // `\` means from the global namespace
         $stmt = $this->db->prepare($query);     // prepare the SQL  (stmt stays for statement)
         $stmt->execute($params);             // execute with parameters
         return $stmt;
