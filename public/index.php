1cffe0edfdf3e73c2927fcbf96b0b804641cb221
Marco Vavassori <mv@marcovavassori.com>
2025-06-03 10:40:23 +0200

    starting point for router

--- public/index.php
+++ public/index.php
@@ -5,3 +5,5 @@ $requestUri = $_SERVER['REQUEST_URI'];
 $requestMethod = $_SERVER['REQUEST_METHOD'];
 
 // sanitize the request uri
+echo "requestUri: $requestUri\n";
+echo "requestMethod: $requestMethod\n";
